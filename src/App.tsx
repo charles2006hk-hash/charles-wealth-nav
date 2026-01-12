8f56d218807694a77498fd30b59487bfe7f2e767
charles2006hk-hash <charles2006hk@gmail.com>
2026-01-12 16:36:00 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,15 +105,6 @@ interface DocConfig {
   statementDateEnd?: string;
 }
 
-interface InsurancePolicy {
-    name: string;
-    totalPaid: number;
-    note: string;
-    lastPaid: string;
-    endYear: number | null;
-    rawMerchant: string;
-}
-
 // --- 3. 常數與圖示 ---
 const ICONS = {
   Home: () => <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>,

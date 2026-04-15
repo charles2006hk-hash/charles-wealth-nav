4b23e8abc43206416440ee129ffa5436c528d865
charles2006hk-hash <charles2006hk@gmail.com>
2026-04-15 14:01:37 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+console.log("Firebase 設定檢查：", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
